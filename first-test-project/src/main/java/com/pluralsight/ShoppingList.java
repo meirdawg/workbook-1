dc3e3be65a0cbce14cc7b12fdcd1cfa1c5684511
Jameir(Meirdawg) <jameireubanks@gmail.com>
2025-04-09 13:13:51 -0400

    Created a shopping list

---
+++ first-test-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("ShoppingList: 5 Donuts,Glasses, Dogleash,noseplug,turtle,avocados,paper,waterbottle,polkadotsocks,vitamins ");
+
+    }
+}
